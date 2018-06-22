9700f6f5ef5f90fe021b5ecbdc1e380a29bb8832
Mike <lanselot777hailander@gmail.com>
2018-06-22 12:02:57 -0400

    Ciclos en Java

---
+++ CicloDoWhile/src/ciclos/CicloDoWhile.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ciclos;
+
+/**
+ *
+ * @author mike
+ */
+public class CicloDoWhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int contador = 0;
+        int limite = 10;
+        do{
+            System.out.println("contador = "+contador);
+            contador++;
+        }while(contador < limite);
+    }
+    
+}
