ffa0fde0562e092163ad39adfa6bda6f222cd166
Mike <lanselot777hailander@gmail.com>
2018-06-22 15:07:04 -0400

    Creacion de clases

---
+++ CreacionClasesObjetos/src/personas/Persona.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personas;
+
+/**
+ *
+ * @author mike
+ */
+public class Persona {
+    //Atributos de una clase
+    //No es necesario asignar valores
+    String nombre;
+    String apellidoPaterno;
+    String apellidoMaterno;
+    
+    //Metodos de la clase
+    //Los usaran os objetos de esta clase
+    public void desplegarNombre(){
+        System.out.println("Nombre : "+nombre);
+        System.out.println("Apellido Paterno : "+apellidoPaterno);
+        System.out.println("Apellido Materno : "+apellidoMaterno);
+    }
+}
