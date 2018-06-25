da9b476629dbac7d54453d6e405a0e6bc6dfeb13
Mike <lanselot777hailander@gmail.com>
2018-06-25 11:11:11 -0400

    Objetos en Java

---
+++ CreacionClasesObjetos/src/personas/PersonaPrueba.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personas;
+
+/**
+ *
+ * @author mike
+ */
+public class PersonaPrueba {
+    public static void main(String args[]){
+        //Creacion de un objeto
+        Persona p1 = new Persona();
+        
+        //Llamando a un metodo del objeto creado
+        System.out.println("Valores por default del objeto Persona");
+        p1.desplegarNombre();
+        
+        //Modificar valores
+        p1.nombre = "Juan";
+        p1.apellidoPaterno = "Esparza";
+        p1.apellidoMaterno = "Lara";
+        
+        //Volvemos a llamar al metodo
+        System.out.println("\nNuevos valores del objeto Persona");
+        p1.desplegarNombre();
+    }
+}
