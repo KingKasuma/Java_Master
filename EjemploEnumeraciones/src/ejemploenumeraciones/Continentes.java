56c234b887b1c4f13cced44d26e991fb9b4b6780
Mike <lanselot777hailander@gmail.com>
2018-06-29 12:17:52 -0400

    Enumeraciones

---
+++ EjemploEnumeraciones/src/ejemploenumeraciones/Continentes.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemploenumeraciones;
+
+/**
+ *
+ * @author mike
+ */
+public enum Continentes {
+    AFRICA(53),
+    EUROPA(46),
+    ASIA(44),
+    AMERICA(34),
+    OCEANIA(14);
+    
+    //Atributo de cada elemento de una enumeracion
+    private final int paises;
+    
+    //Constructor de cade elemento de la enumeracion
+    Continentes(int paises){
+        this.paises = paises;
+    }
+    
+    public int getPaises(){
+        return paises;
+    }
+}
---
+++ EjemploEnumeraciones/src/ejemploenumeraciones/Dias.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemploenumeraciones;
+
+/**
+ *
+ * @author mike
+ */
+public enum Dias {
+    
+//Son valores constantes, por eso van en mayusculas
+    LUNES,
+    MARTES,
+    MIERCOLES,
+    JUEVES,
+    VIERNES,
+    SABADO,
+    DOMINGO
+}
