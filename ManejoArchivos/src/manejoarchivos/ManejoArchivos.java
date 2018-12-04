0deaca6ec1b75f266f319762831dd8338bd076cd
Mike <lanselot777hailander@gmail.com>
2018-12-03 22:26:15 -0400

    Manejo de archivos

---
+++ ManejoArchivos/src/manejoarchivos/ManejoArchivos.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manejoarchivos;
+
+import static utileria.Archivos.*;
+
+public class ManejoArchivos {
+
+    //Nota: Ya debe estar creada la carpeta sobre la que se va a trabajar
+    //Y en caso necesario se deben asignar permisos de escritura a la carpeta
+    private static final String NOMBRE_ARCHIVO = "G:\\Projects\\OWN\\Backend\\Java\\Udemy\\Java_Master\\ManejoArchivos\\pruebaJava.txt";
+      
+    public static void main(String[] args) {
+        //Crear un archivo
+        crearArchivo(NOMBRE_ARCHIVO);
+        
+        //Escribir a un archivo
+        escribirArchivo(NOMBRE_ARCHIVO);
+        
+        //Leer de un archivo
+        leerArchivo(NOMBRE_ARCHIVO);
+        
+        //Anexar informacion a un archivo
+        anexarArchivo(NOMBRE_ARCHIVO);
+        
+        //Leer de un archivo
+        leerArchivo(NOMBRE_ARCHIVO);
+    }
+    
+}
