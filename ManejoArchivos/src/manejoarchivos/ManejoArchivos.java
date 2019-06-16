b835092a00827215a20d61e8261b2aa168e42449
Mike <lanselot777hailander@gmail.com>
2019-06-16 11:06:22 -0400

    Fixing Manejo de archivos

--- ManejoArchivos/src/manejoarchivos/ManejoArchivos.java
+++ ManejoArchivos/src/manejoarchivos/ManejoArchivos.java
@@ -5,29 +5,17 @@
  */
 package manejoarchivos;
 
-import static utileria.Archivos.*;
-
+/**
+ *
+ * @author Mike
+ */
 public class ManejoArchivos {
 
-    //Nota: Ya debe estar creada la carpeta sobre la que se va a trabajar
-    //Y en caso necesario se deben asignar permisos de escritura a la carpeta
-    private static final String NOMBRE_ARCHIVO = "/media/mike/Projects/own/Backend/Java/Udemy/Java_Master/ManejoArchivos/pruebaJava.txt";
-      
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        //Crear un archivo
-        crearArchivo(NOMBRE_ARCHIVO);
-        
-        //Escribir a un archivo
-        escribirArchivo(NOMBRE_ARCHIVO);
-        
-        //Leer de un archivo
-        leerArchivo(NOMBRE_ARCHIVO);
-        
-        //Anexar informacion a un archivo
-        anexarArchivo(NOMBRE_ARCHIVO);
-        
-        //Leer de un archivo
-        leerArchivo(NOMBRE_ARCHIVO);
+        // TODO code application logic here
     }
     
 }
--- ManejoArchivos/src/utileria/Archivos.java
+++ ManejoArchivos/src/utileria/Archivos.java
@@ -5,63 +5,10 @@
  */
 package utileria;
 
-import java.io.*;
-
+/**
+ *
+ * @author Mike
+ */
 public class Archivos {
-    public static void crearArchivo(String nombreArchivo){
-        File archivo = new File(nombreArchivo);
-        try{
-            PrintWriter salida = new PrintWriter(new FileWriter(archivo));
-            salida.close();
-            System.out.println("El arvhico se ha creado correctamente\n");
-        }catch(IOException ex){
-            ex.printStackTrace();
-        }
-    }
     
-    public static void escribirArchivo(String nombreArchivo){
-        File archivo = new File(nombreArchivo);
-        try{
-            PrintWriter salida = new PrintWriter(new FileWriter(archivo));
-            String contenido = "Contenido a escribir en el archivo";
-            salida.println(contenido);
-            salida.println();
-            salida.println("Fin de escritura");
-            salida.close();
-            System.out.println("Se ha escrito correctamente al archivo\n");
-        }catch(IOException ex){
-            ex.printStackTrace();
-        }
-    }
-    
-    public static void leerArchivo(String nombreArchivo){
-        File archivo = new File(nombreArchivo);
-        try{
-            BufferedReader entrada = new BufferedReader(new FileReader(archivo));
-            String lectura;
-            lectura = entrada.readLine();
-            while(lectura != null){
-                System.out.println("Lecutra: "+lectura);
-                lectura = entrada.readLine();
-            }
-            entrada.close();
-        }catch(IOException ex){
-            ex.printStackTrace();
-        }
-    }
-    
-    public static void anexarArchivo(String nombreArchivo){
-        File archivo = new File(nombreArchivo);
-        try{
-            PrintWriter salida = new PrintWriter(new FileWriter(archivo, true));
-            String contenido = "Anexando informacion al archivo";
-            salida.println(contenido);
-            salida.println();
-            salida.println("Fin de anexado");
-            salida.close();
-            System.out.println("Se ha anexado la informacion correctamente\n");
-        }catch(IOException ex){
-            ex.printStackTrace();
-        }
-    }
 }
