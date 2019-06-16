0dde9f3bf9ec33e2ad0b6c943d6d560981b1ca5c
Mike <lanselot777hailander@gmail.com>
2019-06-16 11:15:34 -0400

    Some Fixes

--- ManejoArchivos/src/manejoarchivos/ManejoArchivos.java
+++ ManejoArchivos/src/manejoarchivos/ManejoArchivos.java
@@ -5,17 +5,33 @@
  */
 package manejoarchivos;
 
+import static utileria.Archivos.*;
+
 /**
  *
  * @author Mike
  */
 public class ManejoArchivos {
 
-    /**
-     * @param args the command line arguments
-     */
+    //Nota: Ya debe estar creada la carpeta sobrela que se va a trabajar
+    //Y en caso necesario se deben asignar permisos de escritura a la carpeta
+    private static final String NOMBRE_ARCHIVO = "/media/mike/Projects/own/Backend/Java/Udemy/Java_Master/ManejoArchivos/pruebaJava.txt";
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Crear un archivo
+        crearArchivo(NOMBRE_ARCHIVO);
+        
+        //Escribir a un archivo
+        escribirArchivo(NOMBRE_ARCHIVO);
+        
+        //Leer de un archivo
+        leerArchivo(NOMBRE_ARCHIVO);
+        
+        //Anexar informacion a un archivo
+        anexarArchivo(NOMBRE_ARCHIVO);
+        
+        //Leer de un archivo
+        leerArchivo(NOMBRE_ARCHIVO);
     }
     
 }
